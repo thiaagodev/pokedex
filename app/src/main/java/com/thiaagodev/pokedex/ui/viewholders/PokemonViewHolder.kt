08b1f247acc93d303bf8251821f01b558fd52298
Thiago Ferreira <thiaago.dev@gmail.com>
2023-03-11 15:18:54 -0300

    change: carregando gifs na listagem

--- app/src/main/java/com/thiaagodev/pokedex/ui/viewholders/PokemonViewHolder.kt
+++ app/src/main/java/com/thiaagodev/pokedex/ui/viewholders/PokemonViewHolder.kt
@@ -2,7 +2,7 @@ package com.thiaagodev.pokedex.ui.viewholders
 
 import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.picasso.Picasso
+import com.bumptech.glide.Glide
 import com.thiaagodev.pokedex.databinding.PokemonItemBinding
 import com.thiaagodev.pokedex.service.model.Pokemon
 import java.util.Locale
@@ -11,15 +11,16 @@ class PokemonViewHolder(private val binding: PokemonItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(pokemon: Pokemon, index: Int) {
+        Glide.with(binding.root.context)
+            .asGif()
+            .load(pokemon.getImage(index + 1))
+            .into(binding.imagePokemon)
+
         binding.textPokemonName.text = pokemon.name?.replaceFirstChar {
             if (it.isLowerCase()) it.titlecase(
                 Locale.ROOT
             ) else it.toString()
         }
-
-        Picasso.get()
-            .load(pokemon.getImage(index + 1))
-            .into(binding.imagePokemon)
         binding.textPokemonNumber.text = "#${index + 1}"
         binding.container.setBackgroundColor(Color.parseColor(pokemon.primaryTypeColor))
     }
